2bf984113ff09d63d2cac3577d6d57e226071d94
GODELTECH\a.pashkun <a.pashkun@godeltech.com>
2021-11-23 20:00:01 +0300

    Deleted unused code. Added BookController and BookModel.cs in solution. Installed Fine Code Coverage for the further use.

---
+++ src/Astoneti.Microservice.BookLibrary/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Astoneti.Microservice.BookLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<BookModel> GetBooks()
+        {
+            var list = new List<BookModel>()
+            {
+                new BookModel(){ Id = 1, Title = "My First boob", Author = "Anton Pashkun" },
+                new BookModel(){ Id = 1, Title = "Code First", Author = "Anton Pashkun" },
+                new BookModel(){ Id = 1, Title = "How to become a developer from scratch", Author = "Anton Pashkun" }
+            };
+            return list;
+        }
+    }
+}
