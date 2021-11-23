fd1cf3435fe3a82ae748acd5f93981f911f857c4
GODELTECH\a.pashkun <a.pashkun@godeltech.com>
2021-11-23 17:56:17 +0300

    Add Solution structure

---
+++ test/Astoneti.Microservice.BookLibrary.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Astoneti.Microservice.BookLibrary.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
